03acd77a96eac3d91024376af51e14bee8313b2f
artem_zagovelev <mexell96@gmail.com>
2023-06-06 14:57:36 +0300

    Add checkbox for post

--- src/pages/Todos/Todos.tsx
+++ src/pages/Todos/Todos.tsx
@@ -1,10 +1,58 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Checkbox, List } from "antd";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
+
+import { EStatus, ITodo } from "./types";
 
 const Todos: FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([
+    {
+      id: "1",
+      text: "Racing car sprays burning fuel into crowd.",
+      status: EStatus.InProgress,
+      isChecked: false,
+    },
+    {
+      id: "2",
+      text: "Japanese princess to wed commoner.",
+      status: EStatus.Ready,
+      isChecked: true,
+    },
+    {
+      id: "3",
+      text: "Australian walks 100km after outback crash.",
+      status: EStatus.InProgress,
+      isChecked: false,
+    },
+  ]);
+
+  const handleChange = (e: CheckboxChangeEvent, id: string) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id
+        ? {
+            ...todo,
+            isChecked: !todo.isChecked,
+            status: todo.isChecked ? EStatus.Ready : EStatus.InProgress,
+          }
+        : todo
+    );
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div>
-      <span>Todos</span>
-    </div>
+    <List
+      bordered
+      dataSource={todos}
+      renderItem={(todo) => (
+        <List.Item>
+          <Checkbox
+            onChange={(e) => handleChange(e, todo.id)}
+            checked={todo.isChecked}
+          />
+          <p>{todo.text}</p>
+        </List.Item>
+      )}
+    />
   );
 };
 
